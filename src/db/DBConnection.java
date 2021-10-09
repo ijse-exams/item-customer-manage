99c53ac9eed3334b8b326c5f973382deb750e3a9
YohanSamitha <yohansamitha123@gmail.com>
2021-10-09 10:42:14 +0530

    crete database connection.

---
+++ src/db/DBConnection.java
@@ -0,0 +1,34 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) IJSE-exams. All rights reserved.
+ *  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+package db;
+/*
+ * @author Yohan Samitha
+ * @since 10/9/2021
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    static DBConnection dbConnection;
+    Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/train_stationMS", "root", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection == null) ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
